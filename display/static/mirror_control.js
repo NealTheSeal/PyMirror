b77ab2efeb92d3f57dca9aaf28c2999f1e67b44f
Andrew <bossman@localhost.localdomain>
2017-05-14 15:41:23 -0500

    random old changes

--- display/static/mirror_control.js
+++ display/static/mirror_control.js
@@ -1,27 +1,52 @@
-$(document).ready(function()
-{
-    setInterval(time.show("#time"), 1000);
-	calendar.show("#calendar");
-});
+// $(document).ready(function()
+// {
+//     setInterval(time.show("#time"), 1000);
+// 	calendar.show("#calendar");
+// });
 
-$(window).resize(function() {
-	// do nothing
-});
+// $(window).resize(function() {
+// 	// do nothing
+// });
 
 
-var time = {
+// var time = {
 
-    show: function(target) {
+//     show: function(target) {
 
-        var that = this;
+//         var that = this;
 
-		var minutes = this.date.getMinutes();	
-		if (minutes < 10)
-		{
-			minutes = "0" + minutes;
-		}
+// 		var minutes = this.date.getMinutes();	
+// 		if (minutes < 10)
+// 		{
+// 			minutes = "0" + minutes;
+// 		}
 
-        $(target).html(this.date.getHours() + ":" + minutes);
-		alert("here")
-    }
-};
+//         $(target).html(this.date.getHours() + ":" + minutes);
+// 		alert("here")
+//     }
+// };
+
+// time.show("#time");
+
+function showTime(target) {
+
+	var date = new Date();
+
+	var minutes = date.getMinutes();	
+	if (minutes < 10)
+	{
+		minutes = "0" + minutes;
+	}
+
+	var time = date.getHours() + ":" + minutes + ":" + date.getSeconds();
+
+	// $(target).html(this.date.getHours() + ":" + minutes);
+	document.getElementById("time").innerHTML = time;
+
+	// setInterval(function(){
+	// 	showTime("#time")
+	// },3000);
+}
+
+showTime("#time");
+
